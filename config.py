472c4ec3e0f07b209d6dcf1f388227fa294af1ea
ATOMIC-00 <132604794+ATOMIC-00@users.noreply.github.com>
2024-01-23 23:22:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "") #⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") #⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #⚠️ Required
+    API_ID    = os.environ.get("API_ID", "20263428") #⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "f9dc42564f6e6eb9920912b09f729e3f"") #⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6653409043:AAEXTavYE2PWxuZMe_FlRuYng7fbxtU5rGM") #⚠️ Required
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","Snow_User_Data")     
-    DB_URL  = os.environ.get("DB_URL","") #⚠️ Required
+    DB_NAME = os.environ.get("DB_NAME","Ai_Hoshino")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://Luciferbin:9339119160@cluster0.kmbiojj.mongodb.net/?retryWrites=true&w=majority") #⚠️ Required
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/3b7050b8b5d918a00b490.jpg")
+    START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/326aaeae6ae3eff807460.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()] #⚠️ Required
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") #⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) #⚠️ Required
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Hentaii_flix") #⚠️ Required Username without @
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001944217427")) #⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 
